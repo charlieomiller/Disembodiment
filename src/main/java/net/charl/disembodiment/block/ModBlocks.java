0becb27e28d7d49228ae45dfa34cc762cb4b0727
charlieomiller <COM102103@gmail.com>
2025-09-17 12:12:55 -0400

    Initial commit

---
+++ src/main/java/net/charl/disembodiment/block/ModBlocks.java
@@ -0,0 +1,37 @@
+package net.charl.disembodiment.block;
+
+import net.charl.disembodiment.Disembodiment;
+import net.charl.disembodiment.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, Disembodiment.MOD_ID);
+
+    public static final RegistryObject<Block> DEMATERIALIZER = registerBlock("dematerializer",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.GOLD_BLOCK)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
---
+++ src/main/java/net/charl/disembodiment/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.charl.disembodiment.item;
+
+import net.charl.disembodiment.Disembodiment;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Disembodiment.MOD_ID);
+
+    public static final RegistryObject<Item> INKOR = ITEMS.register("inkor",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
